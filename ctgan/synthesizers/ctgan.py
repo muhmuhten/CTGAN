c8f0b0ba20b935a6be10c3be23a3123053703b82
Julie Fang <julie.fang@merckgroup.com>
2021-07-17 10:11:27 +0200

    add private condition

--- ctgan/synthesizers/ctgan.py
+++ ctgan/synthesizers/ctgan.py
@@ -460,26 +460,27 @@ class CTGAN(BaseSynthesizer):
                     # compute discriminator's gradient
                     loss_d = -(torch.mean(y_real) - torch.mean(y_fake))
 
-                    # clamp parameters into [-0.01, 0.01]
-                    for p in discriminator.parameters():
-                        p.data.clamp_(-0.01, 0.01)
+                    if self.private:
+                        # clamp parameters into [-0.01, 0.01]
+                        for p in discriminator.parameters():
+                            p.data.clamp_(-0.01, 0.01)
 
-                    # weight clipping
-                    utils.clip_grad_norm_(discriminator.parameters(), self.clip_coeff)
+                        # weight clipping
+                        utils.clip_grad_norm_(discriminator.parameters(), self.clip_coeff)
 
-                    clipped_grads = {
-                        name: torch.zeros_like(param, dtype=torch.double) for name, param in
-                        discriminator.named_parameters()
-                    }
+                        clipped_grads = {
+                            name: torch.zeros_like(param, dtype=torch.double) for name, param in
+                            discriminator.named_parameters()
+                        }
 
-                    # add noise
-                    for name, param in discriminator.named_parameters():
-                        if param.grad is not None:
-                            noise = torch.DoubleTensor(
-                                clipped_grads[name].size()
-                            ).normal_(0, self.sigma * self.clip_coeff).to(self._device)
-                            clipped_grads[name] += param.grad + noise
-                            param.grad = clipped_grads[name].float()
+                        # add noise
+                        for name, param in discriminator.named_parameters():
+                            if param.grad is not None:
+                                noise = torch.DoubleTensor(
+                                    clipped_grads[name].size()
+                                ).normal_(0, self.sigma * self.clip_coeff).to(self._device)
+                                clipped_grads[name] += param.grad + noise
+                                param.grad = clipped_grads[name].float()
 
                     optimizerD.zero_grad(set_to_none=False)
                     pen.backward(retain_graph=True)
@@ -513,11 +514,6 @@ class CTGAN(BaseSynthesizer):
                 loss_g = -torch.mean(y_fake) + cross_entropy
 
                 optimizerG.zero_grad(set_to_none=False)
-                if self.private:
-                    # Add random noise to loss_g
-                    loss_g += torch.randn(1).item()
-
-                optimizerG.zero_grad()
                 loss_g.backward()
                 optimizerG.step()
 
