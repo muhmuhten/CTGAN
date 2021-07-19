3d399fe6078100e78fe88e6c1edc3418ed094819
Julie Fang <julie.fang@merckgroup.com>
2021-07-19 08:40:44 +0200

    add noise before training D

--- dp/adults.py
+++ dp/adults.py
@@ -7,6 +7,9 @@ from ctgan import CTGANSynthesizer, load_demo
 from sklearn.model_selection import train_test_split
 from utils import convert_adult_ds, eval_dataset
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 if __name__ == '__main__':
 
@@ -24,7 +27,7 @@ if __name__ == '__main__':
         'income'
     ]
 
-    ctgan = CTGANSynthesizer(epochs=500, verbose=True, private=True)
+    ctgan = CTGANSynthesizer(epochs=10, verbose=True, private=True)
     ctgan.fit(data, discrete_columns)
     ctgan.plot_losses(save=True)
 
@@ -35,7 +38,7 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     print('\nTrain on real, test on real')
-    real = eval_dataset(X_train, y_train, X_test, y_test)
+    real, trtr = eval_dataset(X_train, y_train, X_test, y_test)
 
     # evaluate performance using fake data
     samples = ctgan.sample(len(data)) # Synthetic copy
@@ -43,5 +46,16 @@ if __name__ == '__main__':
     X_syn = _samples.drop(['income'], axis=1)
     y_syn = _samples['income']
     print('\nTrain on fake, test on real')
-    fake = eval_dataset(X_syn, y_syn, X_test, y_test)
+    fake, tstr = eval_dataset(X_syn, y_syn, X_test, y_test)
 
+    # plot
+    metrics = ['acc', 'f1 score', 'auroc', 'auprc']
+    plt.figure(figsize=(5, 5))
+    X = np.arange(4)
+    plt.title("TRTR v.s. TSTR")
+    plt.bar(X + 0.00, trtr, width=0.25)
+    plt.bar(X + 0.25, tstr, width=0.25)
+    plt.xticks(X + 0.25, metrics)
+    plt.legend(['TRTR', 'TSTR'])
+    plt.show()
+    plt.savefig('comparison.png')
--- dp/utils.py
+++ dp/utils.py
@@ -36,7 +36,8 @@ def eval_dataset(X, y, X_test, y_test):
     avg_acc, avg_f1, avg_auroc, avg_auprc = avg_acc/N, avg_f1/N, avg_auroc/N, avg_auprc/N
     print(f'Average: acc {round(avg_acc, 4):>5}\t f1 score {round(avg_f1, 4):>5}\t '
           f'auroc {round(avg_auroc, 4):>5}\t auprc {round(avg_auprc, 4):>5}')
-    return history
+
+    return history, (avg_acc, avg_f1, avg_auroc, avg_auprc)
 
 
 def convert_adult_ds(dataset):
