f35c375f17dca516919cc9d0be196d266498ad43
Julie <j47548081@gmail.com>
2021-08-06 09:34:40 +0200

    refactor plot title

--- dp/cervical.py
+++ dp/cervical.py
@@ -65,11 +65,11 @@ if __name__ == '__main__':
     metrics = ['acc', 'f1 score', 'auroc', 'auprc']
     plt.figure(figsize=(10, 5))
     X = np.arange(4)
-    plt.title("Adult Dataset")
+    plt.title("Cervical Dataset")
     plt.bar(X + 0.00, trtr, width=0.25, color='#8FB9AA')
     plt.bar(X + 0.25, tstr_ctgan, width=0.25, color='#F2D096')
     plt.bar(X + 0.50, tstr_dpctgan, width=0.25, color='#ED8975')
     plt.xticks(X + 0.25, metrics)
-    plt.legend(['Real', 'CTGAN', 'DP-CTGAN'], bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.legend(['Real', 'CTGAN', 'DP-CTGAN'])
 
     plt.show()
